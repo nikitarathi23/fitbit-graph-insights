e705f4b584cb2203b7e1d14b75c4ee2149edfa90
Nikita Rathi <nrathi@Nikitas-MacBook-Pro.local>
2019-02-02 21:29:25 +0000

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GraphComponent msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GraphComponent from './components/GraphComponent.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    GraphComponent
   }
 }
 </script>
